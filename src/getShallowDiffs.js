fc608d33e62c93bf2702ed7febcef59a2a23f5f9
Jon Q <hello@jonquach.com>
2018-11-20 17:29:31 -0500

    Tidy up getShallowDiffs function

--- src/getShallowDiffs.js
+++ src/getShallowDiffs.js
@@ -2,17 +2,12 @@
  * Pluck out the differences between previous/next props/state.
  * Useful for debugging component update/renders.
  *
- * @param {Object} previous previousProps/previousState
+ * @param {Object} previous prevProps/prevState
  * @param {Object} next nextProps/nextState
  * @returns {Object} The differences
  */
 function getShallowDiffs(previous: Object, next: Object): Object {
-  const diffs = Object.keys(previous).reduce((diffs, key) => {
-    if (previous[key] !== next[key]) {
-      return [...diffs, key]
-    }
-    return diffs
-  }, [])
+  const diffs = Object.keys(previous).filter(key => previous[key] !== next[key])
 
   const previousProps = diffs.reduce((previousProps, key) => {
     previousProps[key] = previous[key]
