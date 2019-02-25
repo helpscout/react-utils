0d862f071c6c7fe8c24821185a8fee40e2a313b3
Jon Q <hello@jonquach.com>
2019-02-25 09:22:50 -0500

    Update Fragment. Remove renderChild

--- src/Fragment.tsx
+++ src/Fragment.tsx
@@ -3,16 +3,18 @@ import * as React from 'react'
 function Fragment(props: any): any {
   const { className, children, selector } = props
 
+  /* istanbul ignore else */
   if (React.Fragment) {
     return <React.Fragment>{children}</React.Fragment>
+    // Can't seem to stub/mock out React.Fragment
   } else {
     return React.createElement(selector, { className }, children)
   }
 }
 
 Fragment.defaultProps = {
-  className: 'ReactUtilsFragment'
-  selector: 'div'
+  className: 'ReactUtilsFragment',
+  selector: 'div',
 }
 
 export default Fragment
